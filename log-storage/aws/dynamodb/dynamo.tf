d2ded016e2a5913e943cb58c7fe53a1e90d7ccea
Chaim Platonov <chaim.platonov@env0.com>
2026-03-08 21:03:58 +0200

    disable prevent destroy

--- log-storage/aws/dynamodb/dynamo.tf
+++ log-storage/aws/dynamodb/dynamo.tf
@@ -46,8 +46,8 @@ resource "aws_dynamodb_table" "deployment_remote_run_logs_table" {
   }
 
   lifecycle {
-    prevent_destroy = true
+    prevent_destroy = false
   }
 
-  deletion_protection_enabled = true
+  deletion_protection_enabled = false
 }
